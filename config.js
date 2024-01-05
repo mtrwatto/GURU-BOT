d7f8cfffe91e0b0b28cae2ff8cbc18a0d0f45fa5
mtrwatto <155443632+mtrwatto@users.noreply.github.com>
2024-01-05 06:17:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923456826606', 'MTR', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
